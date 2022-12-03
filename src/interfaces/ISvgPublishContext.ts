5357b7f3ac972722c593df11d5a54d6e5f9e322d
Nikolay Belykh <nbelyh@gmail.com>
2022-12-03 03:29:38 +0100

    convert root component to class

--- src/interfaces/ISvgPublishContext.ts
+++ src/interfaces/ISvgPublishContext.ts
@@ -4,25 +4,13 @@
 // Nikolay Belykh, nbelyh@gmail.com
 //-----------------------------------------------------------------------
 
-import { IHashService } from "./IHashService";
-import { IViewService } from './IViewService';
-import { IBasicService } from './IBasicService';
 import { IDiagramInfo } from './IDiagramInfo';
-import { ISelectionService } from './ISelectionService';
-import { LinksService } from '../services/LinksService';
-import { IHoverService } from './IHoverService';
+import { IServices } from './IServices';
 
 export interface ISvgPublishContext {
   container: HTMLElement;
   svg: SVGSVGElement;
   events: EventTarget;
   diagram: IDiagramInfo;
-  services: {
-    view?: IViewService;
-    selection?: ISelectionService;
-    links?: LinksService;
-    hover?: IHoverService;
-    hash?: IHashService;
-    [key: string]: IBasicService
-  };
+  services: IServices;
 }
