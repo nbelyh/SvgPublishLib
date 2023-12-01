cea6523f12647563b819f29eb2a9cfe10f99d62f
Nikolay Belykh <nbelyh@gmail.com>
2023-12-01 22:40:20 +0100

    add hsla support

--- src/services/SvgFilters.ts
+++ src/services/SvgFilters.ts
@@ -41,6 +41,43 @@ export class SvgFilters {
         a: 1
       }
     }
+
+    const hslaRegex = /^hsla\(\s*(\d+|\d*\.\d+)\s*,\s*(\d+|\d*\.\d+)%\s*,\s*(\d+|\d*\.\d+)%\s*,\s*(\d+|\d*\.\d+)\s*\)$/;
+    if (hslaRegex.test(input)) {
+
+      const [h, s, l, a] = input.match(/\d+(\.\d+)?/g).map((val, index) => {
+        return index === 0 ? parseFloat(val) // hue
+          : index === 3 ? parseFloat(val) // alpha
+            : parseFloat(val) / 100; // saturation and lightness
+      });
+
+      let c = (1 - Math.abs(2 * l - 1)) * s;
+      let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+      let m = l - c / 2;
+      let r = 0, g = 0, b = 0;
+
+      if (0 <= h && h < 60) {
+        r = c; g = x; b = 0;
+      } else if (60 <= h && h < 120) {
+        r = x; g = c; b = 0;
+      } else if (120 <= h && h < 180) {
+        r = 0; g = c; b = x;
+      } else if (180 <= h && h < 240) {
+        r = 0; g = x; b = c;
+      } else if (240 <= h && h < 300) {
+        r = x; g = 0; b = c;
+      } else if (300 <= h && h < 360) {
+        r = c; g = 0; b = x;
+      }
+
+      return {
+        r: r + m,
+        g: g + m,
+        b: b + m,
+        a: a
+      }
+    }
+
     return null;
   }
 
