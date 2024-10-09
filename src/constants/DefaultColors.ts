07b45aef2b7f34ce3cf238c1904a8d3449a91377
Nikolay Belykh <nbelyh@gmail.com>
2024-10-09 04:01:04 +0200

    fix default colors

--- src/constants/DefaultColors.ts
+++ src/constants/DefaultColors.ts
@@ -1,11 +1,11 @@
 
 export class DefaultColors {
   public static selectionColor = 'rgba(255, 255, 0, 0.8)';
-  public static hoverColor = 'rgba(255, 255, 0, 0.2)';
-  public static hyperlinkColor = 'rgba(0, 0, 255, 0.2)';
+  public static hoverColor = 'rgba(255, 255, 0, 0.3)';
+  public static hyperlinkColor = 'rgba(0, 0, 255, 0.3)';
 
-  public static prevShapeColor = 'rgba(0xA0, 0xFF, 0xFF, 0.9)';
-  public static nextShapeColor = 'rgba(0xFF, 0xA0, 0xFF, 0.9)';
-  public static prevConnColor = 'rgba(0xFF, 0x00, 0x00, 1.0)';
-  public static nextConnColor = 'rgba(0xFF, 0x00, 0x00, 1.0)';
+  public static prevShapeColor = `rgba(160, 255, 255, 0.8)`;
+  public static nextShapeColor = `rgba(255, 160, 255, 0.8)`;
+  public static prevConnColor = `rgba(255, 0, 0, 1.0)`;
+  public static nextConnColor = `rgba(255, 0, 0, 1.0)`;
 }
